25c6f5843ab6ce68d41b9dd3f94a0dd8282a2b8d
MuhammadFadll <65841599+fadelun@users.noreply.github.com>
2021-08-23 16:29:39 +0700

    menyederhanakan beberapa snytax

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const allMargin = ["2px", "1.5%", "3px", "-1px", "-2%"];
-const allScale = ["5deg", "-4deg", "2deg", "-3deg", "-2deg"];
+const allRotate = ["5deg", "-4deg", "2deg", "-3deg", "-2deg"];
 const allColors = ["E9C46A", "06D6A0", "118AB2", "EB886F", "EF476F"];
 const btn = document.querySelector("button");
 const modal = document.querySelector(".modal");
 const exit = document.querySelector(".exit");
 const allNotes = document.querySelector(".all-notes");
 
-// const notes = document.getElementsByClassName("note");
-
+// klik button untuk memunculkan note baru
 btn.addEventListener("click", () => {
   modal.style.display = "flex";
 });
 
+// klik exit untuk keluar
 exit.addEventListener("click", () => {
   inputText = "";
   modal.style.display = "none";
@@ -29,6 +29,7 @@ window.addEventListener("keydown", (event) => {
                       </div>`;
 
       allNotes.innerHTML += note;
+      // function untuk memodifikasi note
       styleNote(allNotes.lastChild);
 
       inputText.value = " ";
@@ -38,40 +39,18 @@ window.addEventListener("keydown", (event) => {
 });
 
 const styleNote = (note) => {
-  // const marginSet = allMargin.map((margin, i) => margin, i).filter((margin, i) => Math.floor(Math.random() * 3) == i);
-
-  const randomNum = Math.floor(Math.random() * 4);
-  // const randomNum02 = Math.floor(Math.random() * 4);
-
+  const randomNum = Math.floor(Math.random() * allMargin.length);
+  const randomNum02 = Math.floor(Math.random() * allColors.length);
+  // set margin
   const marginSet = allMargin.filter((m, i) => i == randomNum);
   note.style.marginLeft = marginSet;
-
-  // allMargin.map((m, i) => {
-  //   if (i == randomNum) {
-  //     note.style.marginLeft = m;
-  //   }
-  // });
-
-  const colorSet = allColors.filter((m, i) => i == randomNum);
+  // set warna
+  const colorSet = allColors.filter((m, i) => i == randomNum02);
   note.style.backgroundColor = "#" + colorSet;
-
-  // allColors.map((m, i) => {
-  //   if (i == randomNum02) {
-  //     console.log(i);
-  //     note.style.backgroundColor = "#" + m;
-  //   }
-  // });
-
-  const scaleSet = allScale.filter((m, i) => i == randomNum);
-  note.style.transform = `rotate(${scaleSet})`;
-
-  // allScale.map((m, i) => {
-  //   if (i == randomNum02) {
-  //     console.log(i);
-  //     note.style.transform = `rotate(${m})`;
-  //   }
-  // });
+  // set rotate
+  const rotateSet = allRotate.filter((m, i) => i == randomNum02);
+  note.style.transform = `rotate(${rotateSet})`;
 };
 
-// const scales = allScale.filter((s, i) => i == 0);
-allColors.forEach((m) => console.log(m));
+// const scales = allRotate.filter((s, i) => i == 0);
+// allColors.forEach((m) => console.log(m));
